0494dfb3f6eef645bc65687317f87bd58aecb3ec
Andrey <andy.gusev@list.ru>
2017-05-19 21:24:56 +0300

    help

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,33 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Command requestedCommand = Command.valueOf(args[0]);
-        requestedCommand.exec(args);
+        if (args.length == 0) {
+            Command.help.exec(args);
+            return;
+        }
+        try {
+            Command requestedCommand = Command.valueOf(args[0]);
+            requestedCommand.exec(args);
+        } catch (EnumConstantNotPresentException e) {
+            Command.help.exec(args);
+        }
     }
 
     private enum Command {
         annotate {
             @Override
             protected void exec(String[] args) {
-                DeconvolutionProgram format = DeconvolutionProgram.valueOf(args[1]);
+                if (args.length < 7) {
+                    Command.help.exec(args);
+                    return;
+                }
+                DeconvolutionProgram format =
+                        DeconvolutionProgram.valueOf(args[1]);
                 Path deconvolutionResultsPath = Paths.get(args[2]);
                 Path theoreticScansTablePath = Paths.get(args[3]);
                 Path outputPath = Paths.get(args[4]);
                 double maxEValue = Double.valueOf(args[5]);
+                double accuracy = Double.valueOf(args[6]);
 
                 try {
                     Iterator<ExperimentalScan> outputIterator =
@@ -32,11 +46,27 @@ public class Main {
                     System.out.println("File read/write error.");
                 }
             }
+
+            @Override
+            protected String getDescription() {
+                return name() + "<program name> <deconvolution file>" +
+                        " <table path> <output path> <max eValue> " +
+                        "<accuracy> - annotate deconvolution results" +
+                        "using the given theoretic scans table. Scans" +
+                        "with eValue above the given maximum are " +
+                        "ignored. The supplied accuracy is used for " +
+                        "mass comparision.";
+            }
         },
 
         count {
             @Override
             protected void exec(String[] args) {
+                if (args.length < 2) {
+                    Command.help.exec(args);
+                    return;
+                }
+
                 int pos = 1;
                 List<Predicate<TheoreticScan>> filters = new ArrayList<>();
 
@@ -75,12 +105,24 @@ public class Main {
                     }
                 }
             }
+
+            @Override
+            protected String getDescription() {
+                return name() + "<filters> <table path> - count scans " +
+                        "with certain properties in a table. Filters:" +
+                        "-evalue_over <value>, -evalue_under <value>," +
+                        "-modified, -not_modified.";
+            }
         },
 
         searchPeaks {
             @Override
             protected void exec(String[] args) {
                 try {
+                    if (args.length < 4) {
+                        Command.help.exec(args);
+                        return;
+                    }
                     Path theoreticScansPath = Paths.get(args[1]);
                     Path outputPath = Paths.get(args[2]);
                     int pos = 3;
@@ -102,11 +144,24 @@ public class Main {
                     System.out.println(e.getMessage());
                 }
             }
+
+            @Override
+            protected String getDescription() {
+                return name() + "<theoretic table path> <output path>" +
+                        "<program outputs> - for each peak list " +
+                        "programs that have found it. Program description" +
+                        "format: <name> <output filepath>.";
+            }
         },
 
         countFound {
             @Override
             protected void exec(String[] args) {
+                if (args.length < 2) {
+                    Command.help.exec(args);
+                    return;
+                }
+
                 int pos = 1;
                 boolean excluding = false;
                 double accuracy = Double.valueOf(args[pos++]);
@@ -142,11 +197,26 @@ public class Main {
                     System.out.println(e.getMessage());
                 }
             }
+
+            @Override
+            protected String getDescription() {
+                return name() + " <accuracy> <table path> " +
+                        "<deconvolution output paths> - to list " +
+                        "peaks that are present in the listed output" +
+                        "files. Output file format: <program> <path>. " +
+                        "Use -exclude option to exclude peaks in some " +
+                        "files.";
+            }
         },
 
         searchFound {
             @Override
             protected void exec(String[] args) {
+                if (args.length < 2) {
+                    Command.help.exec(args);
+                    return;
+                }
+
                 int pos = 1;
                 boolean excluding = false;
                 double accuracy = Double.valueOf(args[pos++]);
@@ -179,19 +249,36 @@ public class Main {
                     List<Peak> exclusivelyFound =
                             Analyzer.searchExclusivelyFound(theoreticTable,
                                     foundBy, notFoundBy, accuracy);
-                    exclusivelyFound.forEach(peak -> {
-                        System.out.printf("%d %c%d\n", peak.getScan().getId(),
-                                peak.getIon().getType(), peak.getIon().getNumber());
-                    });
+                    exclusivelyFound.forEach(peak ->
+                        System.out.printf("%d %c%d\n",
+                                peak.getScan().getId(),
+                                peak.getIon().getType(),
+                                peak.getIon().getNumber())
+                    );
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
+            }
+
+            @Override
+            protected String getDescription() {
+                return name() + " <accuracy> <table path> " +
+                        "<deconvolution output paths> - to search for " +
+                        "peaks that are present in the listed output" +
+                        "files. Output file format: <program> <path>. " +
+                        "Use -exclude option to exclude peaks in some " +
+                        "files.";
             }
         },
 
         spectrum {
             @Override
             protected void exec(String[] args) {
+                if (args.length < 3) {
+                    Command.help.exec(args);
+                    return;
+                }
+
                 Path path = Paths.get(args[1]);
                 int id = Integer.valueOf(args[2]);
                 try {
@@ -213,8 +300,31 @@ public class Main {
                     System.out.println("Error reading table.");
                 }
             }
+
+            @Override
+            protected String getDescription() {
+                return name() + " <table path> <scan id>  - to print " +
+                        "a spectrum of a scan.";
+            }
+        },
+
+        help {
+            @Override
+            protected void exec(String[] args) {
+                System.out.println("Commands:");
+                for (Command command: Command.values()) {
+                    System.out.println(command.getDescription());
+                }
+            }
+
+            @Override
+            protected String getDescription() {
+                return name() + " - show this message.";
+            }
         };
 
         protected abstract void exec(String[] args);
+
+        protected abstract String getDescription();
     }
 }
